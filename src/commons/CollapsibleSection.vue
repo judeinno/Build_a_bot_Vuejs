9ef0d03fc87ac7e6a90d045177b30a2bb6ad1fea
ATUHAIRE JUDE INNOCENT <judeinno@gmail.com.com>
2019-07-02 11:43:07 +0300

    Show how to use slots to inject data into a child component.

---
+++ src/commons/CollapsibleSection.vue
@@ -0,0 +1,31 @@
+<template>
+   <div>
+     <div class="header">
+       <span v-if="open" @click="open = !open">&#x25B2;Collapse</span>
+       <span v-if="!open" @click="open = !open">&#x25Bc;Expand</span>
+     </div>
+     <!-- Toggolable content -->
+     <slot v-if="open">
+       Default content
+      </slot>
+   </div>
+</template>
+
+<script>
+export default {
+  name: 'CollapsibleSection',
+  data() {
+    return {
+      open: true,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.header {
+  background-color: #bbb;
+  padding: 3px;
+   cursor: pointer;
+}
+</style>
