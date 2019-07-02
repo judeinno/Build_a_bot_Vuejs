cbcdf309b117ae56ac8d198919e39d5433a23166
ATUHAIRE JUDE INNOCENT <judeinno@gmail.com.com>
2019-07-02 12:18:25 +0300

    Add links to routed pages and the different syntax

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,10 @@
       <nav>
         <ul>
           <li class="nav-item">
-            <img class="logo" src="./assets/build-a-bot-logo.png" />
-            Build a bot
+            <router-link class="nav-link" :to="{ name: 'Home'}">
+              <img class="logo" src="./assets/build-a-bot-logo.png" />
+              Build a bot
+            </router-link>
           </li>
         </ul>
       </nav>
@@ -66,5 +68,9 @@ ul {
 .logo {
   vertical-align: middle;
   height: 30px;
+}
+.nav-link {
+  text-decoration: none;
+  color: inherit;
 }
 </style>
--- src/home/HomePage.vue
+++ src/home/HomePage.vue
@@ -5,7 +5,7 @@
         aria-hidden="true"/>
     </div>
     <div class="get-started">
-      <a href="">Get Started</a> Building your first robot!
+      <router-link to="/build">Get Started</router-link> Building your first robot!
     </div>
   </div>
 </template>
