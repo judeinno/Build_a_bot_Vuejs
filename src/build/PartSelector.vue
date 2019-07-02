e905713d757e2c0ab7698a27cc2c548d94179971
ATUHAIRE JUDE INNOCENT <judeinno@gmail.com.com>
2019-07-02 12:43:37 +0300

    Navigating to a route through code

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="part" :class="position">
-    <img :src="selectedPart.src" title="arm"/>
+    <img @click="showPartInfo()" :src="selectedPart.src" title="arm"/>
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
     <span class="sale" v-show="selectedPart.onSale">Sale!</span>
@@ -64,7 +64,9 @@ export default {
         this.parts.length,
       );
     },
-
+    showPartInfo() {
+      this.$router.push('/parts');
+    },
   },
 };
 
@@ -96,6 +98,7 @@ export default {
 }
 .part img {
   width:165px;
+  cursor: pointer;
 }
 .top {
   border-bottom: none;
