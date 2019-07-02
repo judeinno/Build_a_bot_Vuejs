3e6b49c16345468798d34acf947b8cd998bffcc8
ATUHAIRE JUDE INNOCENT <judeinno@gmail.com.com>
2019-07-02 13:59:39 +0300

    Show how to use nested routes

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,11 @@ import Router from 'vue-router';
 import HomePage from '../home/HomePage.vue';
 import RobortBuilder from '../build/RobotBuilder.vue';
 import PartInfo from '../parts/PartInfo.vue';
+import BrowseParts from '../parts/BrowseParts.vue';
+import RobortHeads from '../parts/RobortHeads.vue';
+import RobortBases from '../parts/RobortBases.vue';
+import RobortArms from '../parts/RobortArms.vue';
+import RobortTorsos from '../parts/RobortTorsos.vue';
 
 Vue.use(Router);
 
@@ -17,6 +22,33 @@ export default new Router({
       path: '/build',
       name: 'Build',
       component: RobortBuilder,
+    },
+    {
+      path: '/parts/browse',
+      name: 'BrowseParts',
+      component: BrowseParts,
+      children: [
+        {
+          path: 'heads',
+          name: 'BrowseHeads',
+          component: RobortHeads,
+        },
+        {
+          path: 'bases',
+          name: 'BrowseBases',
+          component: RobortBases,
+        },
+        {
+          path: 'arms',
+          name: 'BrowseArms',
+          component: RobortArms,
+        },
+        {
+          path: 'torsors',
+          name: 'BrowseTorsos',
+          component: RobortTorsos,
+        },
+      ],
     },
     {
       path: '/parts/:partType/:id',
