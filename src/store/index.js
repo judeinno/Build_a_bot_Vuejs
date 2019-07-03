e5935b7f6949a796217f9398bba24f2924fdfd6d
ATUHAIRE JUDE INNOCENT <judeinno@gmail.com.com>
2019-07-03 11:12:17 +0300

    Show hoe to use getters to encapsulate logic in the store

--- src/store/index.js
+++ src/store/index.js
@@ -12,4 +12,9 @@ export default new Vuex.Store({
       state.cart.push(robort);
     },
   },
+  getters: {
+    cartSaleItems(state) {
+      return state.cart.filter(item => item.head.onSale);
+    },
+  },
 });
