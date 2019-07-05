54e571bfe8674b1c28b7f8ef77750346a5983335
ATUHAIRE JUDE INNOCENT <judeinno@gmail.com.com>
2019-07-05 11:50:44 +0300

    Show how to use map mutations

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -54,7 +54,7 @@
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapActions, mapMutations } from 'vuex';
 import createHookMixin from './create-hook-mixin';
 import PartSelector from './PartSelector.vue';
 import CollapsibleSection from '../commons/CollapsibleSection.vue';
@@ -99,6 +99,7 @@ export default {
   },
   methods: {
     ...mapActions('roborts', ['getParts', 'addRobortToCart']),
+    ...mapMutations('roborts',  ['someMutations']),
     addToCart() {
       const robot = this.selectedRobot;
       const cost = robot.head.cost + robot.leftArm.cost
